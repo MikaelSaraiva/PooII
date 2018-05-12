b455cc4096a4e9eb68795bd4d560c7b4f775992d
MikaelSaraiva <mikaelsaraiva@gmail.com>
2018-05-12 19:22:04 -0300

    Criando keylistener

--- src/Exercicio_2/Frame.java
+++ src/Exercicio_2/Frame.java
@@ -15,9 +15,9 @@ import javax.swing.JButton;
 import javax.swing.JDesktopPane;
 import javax.swing.JFrame;
 import javax.swing.JInternalFrame.JDesktopIcon;
-import javax.swing.border.Border;
+import javax.swing.JLabel;
 import javax.swing.JPanel;
-import javax.swing.SwingConstants;
+import javax.swing.JTextArea;
 
 public class Frame extends JFrame {
 
@@ -32,7 +32,8 @@ public class Frame extends JFrame {
 	private JButton[] botoesLinha3;
 	private JButton[] botoesLinha4;
 	private JButton[] botoesLinha5;
-	private GridLayout layout;
+	private JTextArea textArea;
+	private JLabel informacao;
 
 	public Frame() {
 		super("Typing Applicatiom");
@@ -40,9 +41,9 @@ public class Frame extends JFrame {
 		JPanel mainPanel = new JPanel();
 		JPanel textPanel = new JPanel();
 		JPanel keybordPanel = new JPanel();
-		layout = new GridLayout(3, 14, 2, 2);
 
 		keybordPanel.setLayout(null);
+		textPanel.setLayout(new FlowLayout());
 
 		mainPanel.setLayout(new GridLayout(2, 1));
 
@@ -51,6 +52,13 @@ public class Frame extends JFrame {
 		mainPanel.add(keybordPanel);
 
 		keybordPanel.setBackground(Color.BLACK);
+
+		informacao = new JLabel(
+				"<html>Type some text using your keyboard. The keys you press will be highlighted and the text will be displayed. <br>Note: Clicking the buttons with your mouse will not perform any action.<html>");
+		textPanel.add(informacao);
+
+		textArea = new JTextArea(20, 60);
+		textPanel.add(textArea);
 
 		int larguraAltura = 50;
 		int espacamento = larguraAltura + 2;
@@ -70,22 +78,37 @@ public class Frame extends JFrame {
 			keybordPanel.add(botoesLinha1[ultimoBotao]);
 		}
 
-		// botoesLinha2 = new JButton[teclasLinha2.length];
-		// for (int i = 0; i < teclasLinha2.length; i++) {
-		// botoesLinha2[i] = new JButton(teclasLinha2[i]);
-		// botoesLinha2[i].setHorizontalTextPosition(SwingConstants.RIGHT);
-		// botoesLinha2[i].setBounds((espacamento * i), entreLinha * 2, larguraAltura,
-		// larguraAltura);
-		// keybordPanel.add(botoesLinha2[i]);
-		// }
-		// botoesLinha3 = new JButton[teclasLinha3.length];
-		// for (int i = 0; i < teclasLinha3.length; i++) {
-		// botoesLinha3[i] = new JButton(teclasLinha3[i]);
-		// botoesLinha3[i].setHorizontalTextPosition(SwingConstants.RIGHT);
-		// botoesLinha3[i].setBounds((espacamento * i), entreLinha * 3, larguraAltura,
-		// larguraAltura);
-		// keybordPanel.add(botoesLinha3[i]);
-		// }
+		botoesLinha2 = new JButton[teclasLinha2.length];
+		for (int i = 1; i < teclasLinha2.length; i++) {
+			botoesLinha2[i] = new JButton(teclasLinha2[i]);
+			botoesLinha2[0] = new JButton(teclasLinha2[0]);
+			botoesLinha2[i].setFont(new Font("Serif", Font.PLAIN, 14));
+			botoesLinha2[0].setFont(new Font("Serif", Font.PLAIN, 14));
+			botoesLinha2[i].setBounds((espacamento * i + 25), entreLinha * 2, larguraAltura, larguraAltura);
+			botoesLinha2[0].setBounds((espacamento * 0), entreLinha * 2, (larguraAltura + (larguraAltura / 2)),
+					larguraAltura);
+			keybordPanel.add(botoesLinha2[i]);
+			keybordPanel.add(botoesLinha2[0]);
+
+		}
+		botoesLinha3 = new JButton[teclasLinha3.length];
+		for (int i = 1; i < teclasLinha3.length - 1; i++) {
+			int ultimoBotao = teclasLinha3.length - 1;
+			botoesLinha3[i] = new JButton(teclasLinha3[i]);
+			botoesLinha3[0] = new JButton(teclasLinha3[0]);
+			botoesLinha3[ultimoBotao] = new JButton(teclasLinha3[ultimoBotao]);
+			botoesLinha3[i].setFont(new Font("Serif", Font.PLAIN, 14));
+			botoesLinha3[0].setFont(new Font("Serif", Font.PLAIN, 14));
+			botoesLinha3[ultimoBotao].setFont(new Font("Serif", Font.PLAIN, 14));
+			botoesLinha3[i].setBounds((espacamento * i + 25), entreLinha * 3, larguraAltura, larguraAltura);
+			botoesLinha3[0].setBounds((espacamento * 0), entreLinha * 3, (larguraAltura + (larguraAltura / 2)),
+					larguraAltura);
+			botoesLinha3[ultimoBotao].setBounds((espacamento * ultimoBotao + 25), entreLinha * 3, larguraAltura * 2,
+					larguraAltura);
+			keybordPanel.add(botoesLinha3[i]);
+			keybordPanel.add(botoesLinha3[0]);
+			keybordPanel.add(botoesLinha3[ultimoBotao]);
+		}
 
 		botoesLinha4 = new JButton[teclasLinha4.length];
 		for (int i = 1; i < teclasLinha4.length - 1; i++) {
@@ -97,8 +120,7 @@ public class Frame extends JFrame {
 			botoesLinha4[0].setFont(new Font("Serif", Font.PLAIN, 14));
 			botoesLinha4[ultimoBotao].setFont(new Font("Serif", Font.PLAIN, 14));
 			botoesLinha4[i].setBounds(espacamento * (i + 1) - 2, entreLinha * 4, larguraAltura, larguraAltura);
-			botoesLinha4[ultimoBotao].setBounds((espacamento * (ultimoBotao + 1) - 2), entreLinha * 4, larguraAltura,
-					larguraAltura);
+			botoesLinha4[ultimoBotao].setBounds((espacamento * 12 + 25), entreLinha * 4, larguraAltura, larguraAltura);
 			botoesLinha4[0].setBounds(espacamento * 0, entreLinha * 4, larguraAltura * 2, larguraAltura);
 			keybordPanel.add(botoesLinha4[i]);
 			keybordPanel.add(botoesLinha4[0]);
@@ -106,6 +128,25 @@ public class Frame extends JFrame {
 
 		}
 
+		botoesLinha5 = new JButton[teclasLinha5.length];
+		botoesLinha5[0] = new JButton(teclasLinha5[0]);
+		botoesLinha5[0].setBounds(espacamento * (3 + 1) - 2, entreLinha * 5, larguraAltura * 6, larguraAltura);
+		keybordPanel.add(botoesLinha5[0]);
+
+		botoesLinha5[1] = new JButton(teclasLinha5[1]);
+		botoesLinha4[1].setFont(new Font("Serif", Font.PLAIN, 14));
+		botoesLinha5[1].setBounds((espacamento * 11 + 25), entreLinha * 5, larguraAltura, larguraAltura);
+		keybordPanel.add(botoesLinha5[1]);
+
+		botoesLinha5[2] = new JButton(teclasLinha5[2]);
+		botoesLinha4[2].setFont(new Font("Serif", Font.PLAIN, 14));
+		botoesLinha5[2].setBounds((espacamento * 12 + 25), entreLinha * 5, larguraAltura, larguraAltura);
+		keybordPanel.add(botoesLinha5[2]);
+
+		botoesLinha5[3] = new JButton(teclasLinha5[3]);
+		botoesLinha4[3].setFont(new Font("Serif", Font.PLAIN, 14));
+		botoesLinha5[3].setBounds((espacamento * 13 + 25), entreLinha * 5, larguraAltura, larguraAltura);
+		keybordPanel.add(botoesLinha5[3]);
 	}
 
 }
--- src/Exercicio_2/Main.java
+++ src/Exercicio_2/Main.java
@@ -8,6 +8,7 @@ public class Main {
 		Frame frame = new Frame();
 		frame.setSize(800,800);
 		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		frame.setLocationRelativeTo(frame);
 		frame.setVisible(true);
 		
 	}
