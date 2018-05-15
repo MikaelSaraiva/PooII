aa00311d51fbef6be40510fd490d78b916a05678
MikaelSaraiva <mikaelsaraiva@gmail.com>
2018-05-15 06:35:18 -0300

    To quase lá

--- src/Exercicio_2/Frame.java
+++ src/Exercicio_2/Frame.java
@@ -6,10 +6,15 @@ import java.awt.Font;
 import java.awt.GridLayout;
 import java.awt.event.KeyEvent;
 import java.awt.event.KeyListener;
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileReader;
+import java.io.IOException;
 
 import javax.swing.JButton;
 import javax.swing.JFrame;
 import javax.swing.JLabel;
+import javax.swing.JOptionPane;
 import javax.swing.JPanel;
 import javax.swing.JTextArea;
 
@@ -30,7 +35,9 @@ public class Frame extends JFrame implements KeyListener {
 	private JButton[] botoesLinha5;
 	private JTextArea textArea;
 	private JLabel informacao;
+	private JLabel pangramas;
 	private Color buttonColor = null;
+	private Reconhecimento[] reconhecimento;
 
 	public Frame() {
 		super("Typing Applicatiom");
@@ -52,10 +59,20 @@ public class Frame extends JFrame implements KeyListener {
 				"<html>Type some text using your keyboard. The keys you press will be highlighted and the text will be displayed. <br>Note: Clicking the buttons with your mouse will not perform any action.<html>");
 		textPanel.add(informacao);
 
-		textArea = new JTextArea(20, 60);
+		textArea = new JTextArea(18, 90);
 		textArea.setLineWrap(true);
+		textArea.setFont(new Font("MONOSPACED", Font.PLAIN, 12));
 		textPanel.add(textArea);
 		textArea.addKeyListener(this);
+
+		try (BufferedReader reader = new BufferedReader(
+				new FileReader(new File("/home/saraiva/eclipse-workspace/PooII/src/Exercicio_2/pangramas.txt")))) {
+			String linha = reader.readLine();
+			pangramas = new JLabel("<html>Please write the text below, at the end press Enter:<br> <html>" + linha);
+			textPanel.add(pangramas);
+		} catch (IOException ex) {
+			ex.printStackTrace();
+		}
 
 		int larguraAltura = 50;
 		int espacamento = larguraAltura + 2;
@@ -145,15 +162,266 @@ public class Frame extends JFrame implements KeyListener {
 		botoesLinha5[3].setBounds((espacamento * 13 + 25), entreLinha * 5, larguraAltura, larguraAltura);
 		keybordPanel.add(botoesLinha5[3]);
 
-		addKeyListener(this);
 	}
 
 	@Override
 	public void keyTyped(KeyEvent e) {
+		int key = e.getKeyCode();
+
+		JButton button = null;
+
+		switch (key) {
+		// TODO Eventos da primeira linha de botoes
+		case KeyEvent.VK_DEAD_TILDE:
+			button = botoesLinha1[0];
+			break;
+		case KeyEvent.VK_1:
+			button = botoesLinha1[1];
+			break;
+
+		case KeyEvent.VK_2:
+			button = botoesLinha1[2];
+			break;
+
+		case KeyEvent.VK_3:
+			button = botoesLinha1[3];
+			break;
+
+		case KeyEvent.VK_4:
+			button = botoesLinha1[4];
+			break;
+
+		case KeyEvent.VK_5:
+			button = botoesLinha1[5];
+			break;
+
+		case KeyEvent.VK_6:
+			button = botoesLinha1[6];
+			break;
+
+		case KeyEvent.VK_7:
+			button = botoesLinha1[7];
+			break;
+
+		case KeyEvent.VK_8:
+			button = botoesLinha1[8];
+			break;
+
+		case KeyEvent.VK_9:
+			button = botoesLinha1[9];
+			break;
+
+		case KeyEvent.VK_0:
+			button = botoesLinha1[10];
+			break;
+
+		case KeyEvent.VK_MINUS:
+			button = botoesLinha1[11];
+			break;
+
+		case KeyEvent.VK_EQUALS:
+			if ((e.getModifiers() & KeyEvent.SHIFT_MASK) > 0) {
+				button = botoesLinha1[12];
+			}
+			break;
+
+		case KeyEvent.VK_BACK_SPACE:
+			button = botoesLinha1[13];
+			break;
+
+		// TODO Eventos da segunda linha de botoes
+		case KeyEvent.VK_TAB:
+			button = botoesLinha2[0];
+			break;
+
+		case KeyEvent.VK_Q:
+			button = botoesLinha2[1];
+			break;
+
+		case KeyEvent.VK_W:
+			button = botoesLinha2[2];
+			break;
+
+		case KeyEvent.VK_E:
+			button = botoesLinha2[3];
+			break;
+
+		case KeyEvent.VK_R:
+			button = botoesLinha2[4];
+			break;
+
+		case KeyEvent.VK_T:
+			button = botoesLinha2[5];
+			break;
+
+		case KeyEvent.VK_Y:
+			button = botoesLinha2[6];
+			break;
+
+		case KeyEvent.VK_U:
+			button = botoesLinha2[7];
+			break;
+
+		case KeyEvent.VK_I:
+			button = botoesLinha2[8];
+			break;
+
+		case KeyEvent.VK_O:
+			button = botoesLinha2[9];
+			break;
+
+		case KeyEvent.VK_P:
+			button = botoesLinha2[10];
+			break;
+
+		case KeyEvent.VK_OPEN_BRACKET:
+			button = botoesLinha2[11];
+			break;
+
+		case KeyEvent.VK_CLOSE_BRACKET:
+			button = botoesLinha2[12];
+			break;
+
+		case KeyEvent.VK_SLASH:
+			if ((e.getModifiers() & KeyEvent.SHIFT_MASK) > 0) {
+				button = botoesLinha4[10];
+			} else {
+				button = botoesLinha2[13];
+			}
+			break;
+
+		// TODO Eventos da terceira linha
+		case KeyEvent.VK_CAPS_LOCK:
+			button = botoesLinha3[0];
+			break;
+
+		case KeyEvent.VK_A:
+			button = botoesLinha3[1];
+			break;
+
+		case KeyEvent.VK_S:
+			button = botoesLinha3[2];
+			break;
+
+		case KeyEvent.VK_D:
+			button = botoesLinha3[3];
+			break;
+
+		case KeyEvent.VK_F:
+			button = botoesLinha3[4];
+			break;
+
+		case KeyEvent.VK_G:
+			button = botoesLinha3[5];
+			break;
+
+		case KeyEvent.VK_H:
+			button = botoesLinha3[6];
+			break;
+
+		case KeyEvent.VK_J:
+			button = botoesLinha3[7];
+			break;
+
+		case KeyEvent.VK_K:
+			button = botoesLinha3[8];
+			break;
+
+		case KeyEvent.VK_L:
+			button = botoesLinha3[9];
+			break;
+
+		case KeyEvent.VK_SEMICOLON:
+			button = botoesLinha3[10];
+			break;
+
+		case KeyEvent.VK_QUOTE:
+			if ((e.getModifiers() & KeyEvent.SHIFT_MASK) > 0)
+				button = botoesLinha3[11];
+			break;
+
+		case KeyEvent.VK_ENTER:
+			button = botoesLinha3[12];
+			break;
+
+		// TODO Eventos da quarta linha de botoes
+		case KeyEvent.VK_SHIFT:
+			button = botoesLinha4[0];
+			break;
+
+		case KeyEvent.VK_Z:
+			button = botoesLinha4[1];
+			break;
+
+		case KeyEvent.VK_X:
+			button = botoesLinha4[2];
+			break;
+
+		case KeyEvent.VK_C:
+			button = botoesLinha4[3];
+			break;
+
+		case KeyEvent.VK_V:
+			button = botoesLinha4[4];
+			break;
+
+		case KeyEvent.VK_B:
+			button = botoesLinha4[5];
+			break;
+
+		case KeyEvent.VK_N:
+			button = botoesLinha4[6];
+			break;
+
+		case KeyEvent.VK_M:
+			button = botoesLinha4[7];
+			break;
+
+		case KeyEvent.VK_COMMA:
+			button = botoesLinha4[8];
+			break;
+
+		case KeyEvent.VK_PERIOD:
+			button = botoesLinha4[9];
+			break;
+
+		case KeyEvent.VK_UP:
+			button = botoesLinha4[11];
+			break;
+
+		// TODO Eventos da quinta linha de botoes
+		case KeyEvent.VK_SPACE:
+			button = botoesLinha5[0];
+			break;
+
+		case KeyEvent.VK_LEFT:
+			button = botoesLinha5[1];
+			break;
+
+		case KeyEvent.VK_DOWN:
+			button = botoesLinha5[2];
+			break;
+
+		case KeyEvent.VK_RIGHT:
+			button = botoesLinha5[3];
+			break;
+		default:
+			e.consume();
+		}
+
+		if (button != null) {
+			button.setBackground(buttonColor);
+		}
 	}
 
 	@Override
 	public void keyReleased(KeyEvent e) {
+		// Reconhecimento[] reconhecimento = new Reconhecimento[teclasLinha1.length];
+		// botoesLinha1 = new JButton[teclasLinha1.length];
+		// for(int i = 0; i < teclasLinha1.length; i++) {
+		// reconhecimento[i].tc(e, botoesLinha1[i], Color.GRAY);
+		// }
+
 		int key = e.getKeyCode();
 
 		JButton button = null;
@@ -393,6 +661,9 @@ public class Frame extends JFrame implements KeyListener {
 		case KeyEvent.VK_RIGHT:
 			button = botoesLinha5[3];
 			break;
+		default:
+			e.consume();
+			JOptionPane.showMessageDialog(null, "Tecla invalida");
 		}
 
 		if (button != null) {
@@ -402,6 +673,12 @@ public class Frame extends JFrame implements KeyListener {
 
 	@Override
 	public void keyPressed(KeyEvent e) {
+		// Reconhecimento[] reconhecimento = new Reconhecimento[teclasLinha1.length];
+		// botoesLinha1 = new JButton[teclasLinha1.length];
+		// for(int i = 0; i < teclasLinha1.length; i++) {
+		// reconhecimento[i].tc(e, botoesLinha1[i], Color.GRAY);
+		// }
+
 		int key = e.getKeyCode();
 
 		JButton button = null;
@@ -570,7 +847,8 @@ public class Frame extends JFrame implements KeyListener {
 			break;
 
 		case KeyEvent.VK_SEMICOLON:
-			button = botoesLinha3[10];
+			if ((e.getModifiers() & KeyEvent.SHIFT_MASK) > 0)
+				button = botoesLinha3[10];
 			break;
 
 		case KeyEvent.VK_QUOTE:
@@ -643,6 +921,8 @@ public class Frame extends JFrame implements KeyListener {
 		case KeyEvent.VK_RIGHT:
 			button = botoesLinha5[3];
 			break;
+		default:
+			e.consume();
 		}
 
 		if (button != null) {
--- src/Exercicio_2/Reconhecimento.java
+++ src/Exercicio_2/Reconhecimento.java
@@ -1,5 +1,267 @@
 package Exercicio_2;
 
+import java.awt.Color;
+import java.awt.event.KeyEvent;
+import java.io.FileReader;
+
+import javax.swing.JButton;
+import javax.swing.JOptionPane;
+
 public class Reconhecimento {
 
+//	FileReader fileReader = new FileReader("PooII/src/Exercicio_2/Pangramas.txt");
+	
+	public Reconhecimento() {
+		
+	}
+	
+	public void tc(KeyEvent e, JButton buttons, Color color) {
+		JButton button = null;
+		int key = e.getKeyCode();
+		
+		switch (key) {
+		case KeyEvent.VK_DEAD_TILDE:
+			button = buttons;
+			break;
+		case KeyEvent.VK_1:
+			button = buttons;
+			break;
+
+		case KeyEvent.VK_2:
+			button = buttons;
+			break;
+
+		case KeyEvent.VK_3:
+			button = buttons;
+			break;
+
+		case KeyEvent.VK_4:
+			button = buttons;
+			break;
+
+		case KeyEvent.VK_5:
+			button = buttons;
+			break;
+
+		case KeyEvent.VK_6:
+			button = buttons;
+			break;
+
+		case KeyEvent.VK_7:
+			button = buttons;
+			break;
+
+		case KeyEvent.VK_8:
+			button = buttons;
+			break;
+
+		case KeyEvent.VK_9:
+			button = buttons;
+			break;
+
+		case KeyEvent.VK_0:
+			button = buttons;
+			break;
+
+		case KeyEvent.VK_MINUS:
+			button = buttons;
+			break;
+
+		case KeyEvent.VK_EQUALS:
+			if ((e.getModifiers() & KeyEvent.SHIFT_MASK) > 0) {
+				button = buttons;
+			}
+			break;
+
+		case KeyEvent.VK_BACK_SPACE:
+			button = buttons;
+			break;
+
+		// TODO Eventos da segunda linha de botoes
+		case KeyEvent.VK_TAB:
+			button = buttons;
+			break;
+
+		case KeyEvent.VK_Q:
+			button = buttons;
+			break;
+
+		case KeyEvent.VK_W:
+			button = buttons;
+			break;
+
+		case KeyEvent.VK_E:
+			button = buttons;
+			break;
+
+		case KeyEvent.VK_R:
+			button = buttons;
+			break;
+
+		case KeyEvent.VK_T:
+			button = buttons;
+			break;
+
+		case KeyEvent.VK_Y:
+			button = buttons;
+			break;
+
+		case KeyEvent.VK_U:
+			button = buttons;
+			break;
+
+		case KeyEvent.VK_I:
+			button = buttons;
+			break;
+
+		case KeyEvent.VK_O:
+			button = buttons;
+			break;
+
+		case KeyEvent.VK_P:
+			button = buttons;
+			break;
+
+		case KeyEvent.VK_OPEN_BRACKET:
+			button = buttons;
+			break;
+
+		case KeyEvent.VK_CLOSE_BRACKET:
+			button = buttons;
+			break;
+
+		case KeyEvent.VK_SLASH:
+			if ((e.getModifiers() & KeyEvent.SHIFT_MASK) > 0) {
+				button = buttons;
+			} else {
+				button = buttons;
+			}
+			break;
+
+		// TODO Eventos da terceira linha
+		case KeyEvent.VK_CAPS_LOCK:
+			button = buttons;
+			break;
+
+		case KeyEvent.VK_A:
+			button = buttons;
+			break;
+
+		case KeyEvent.VK_S:
+			button = buttons;
+			break;
+
+		case KeyEvent.VK_D:
+			button = buttons;
+			break;
+
+		case KeyEvent.VK_F:
+			button = buttons;
+			break;
+
+		case KeyEvent.VK_G:
+			button = buttons;
+			break;
+
+		case KeyEvent.VK_H:
+			button = buttons;
+			break;
+
+		case KeyEvent.VK_J:
+			button = buttons;
+			break;
+
+		case KeyEvent.VK_K:
+			button = buttons;
+			break;
+
+		case KeyEvent.VK_L:
+			button = buttons;
+			break;
+
+		case KeyEvent.VK_SEMICOLON:
+			button = buttons;
+			break;
+
+		case KeyEvent.VK_QUOTE:
+			if ((e.getModifiers() & KeyEvent.SHIFT_MASK) > 0)
+				button = buttons;
+			break;
+
+		case KeyEvent.VK_ENTER:
+			button = buttons;
+			break;
+
+		// TODO Eventos da quarta linha de botoes
+		case KeyEvent.VK_SHIFT:
+			button = buttons;
+			break;
+
+		case KeyEvent.VK_Z:
+			button = buttons;
+			break;
+
+		case KeyEvent.VK_X:
+			button = buttons;
+			break;
+
+		case KeyEvent.VK_C:
+			button = buttons;
+			break;
+
+		case KeyEvent.VK_V:
+			button = buttons;
+			break;
+
+		case KeyEvent.VK_B:
+			button = buttons;
+			break;
+
+		case KeyEvent.VK_N:
+			button = buttons;
+			break;
+
+		case KeyEvent.VK_M:
+			button = buttons;
+			break;
+
+		case KeyEvent.VK_COMMA:
+			button = buttons;
+			break;
+
+		case KeyEvent.VK_PERIOD:
+			button = buttons;
+			break;
+
+		case KeyEvent.VK_UP:
+			button = buttons;
+			break;
+
+		// TODO Eventos da quinta linha de botoes
+		case KeyEvent.VK_SPACE:
+			button = buttons;
+			break;
+
+		case KeyEvent.VK_LEFT:
+			button = buttons;
+			break;
+
+		case KeyEvent.VK_DOWN:
+			button = buttons;
+			break;
+
+		case KeyEvent.VK_RIGHT:
+			button = buttons;
+			break;
+		default:
+			e.consume();
+			JOptionPane.showMessageDialog(null, "Tecla invalida");
+		}
+
+		if (button != null) {
+			button.setBackground(color);
+		}
+	}
+	
+//	Comparo input a lnha de texto que eu tenho
 }
